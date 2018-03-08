e759d5a975cebf6783a5a93db4e59afd37d26574
pkedu <pkedu@btkang.com>
2018-03-08 17:06:45 +0800

    office 2018-03-08

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,6 +11,25 @@ Page({
       .then(res => {
         this.setData({ categories: res.data })
       })
+  },
+
+  searchHandle () {
+    // console.log(this.data.searchText)
+    this.setData({ shops: [], pageIndex: 0, hasMore: true })
+    this.loadMore()
+  },
+
+  showSearchHandle() {
+    this.setData({ searchShowed: true })
+  },
+  hideSearchHandle() {
+    this.setData({ searchText: '', searchShowed: false })
+  },
+  clearSearchHandle() {
+    this.setData({ searchText: '' })
+  },
+  searchChangeHandle(e) {
+    this.setData({ searchText: e.detail.value })
   }
 })
 
